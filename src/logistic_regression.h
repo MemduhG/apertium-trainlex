1bf189e6e8c8a3b01aab0276e866e32366220281
MemduhG <memduhg@gmail.com>
2019-10-05 16:52:39 +0200

    more logreg method

--- src/logistic_regression.h
+++ src/logistic_regression.h
@@ -1,17 +1,57 @@
 #include <vector>
 
-class regressor{
+class Regressor
+{
 	int num_feats, num_classes;
-	float bias;
+	std::vector<float> bias;
 	std::vector<std::vector<float>> weights;
 public:
-	float predict_value(std::vector<float> features, int label);
+	float predictValue(std::vector<float> features, int label);
+	int predictLabel(std::vector<float> features);
+	std::vector<float> softmax(std::vector<float> features);
 };
 
 
-float regressor::predict_value(std::vector<float> features, int label){
-	float result = bias ;
+float
+Regressor::predictValue(std::vector<float> features, int label)
+{
+	float result = bias[label];
 	for (int i=0; i < num_feats; i++)
-		result += features[i] * weights[label][i] ;
+		result += features[i] * weights[label][i];
 	return result ;
+}
+
+std::vector<float> 
+Regressor::softmax(std::vector<float> features)
+{
+	std::vector<float> scores;
+	float total = 0;
+	float new_score;
+	for (int i=0; i < num_classes; i++)
+	{
+		new_score = predictValue(features, i);
+		scores.push_back(new_score);
+		total += new_score;
+	}
+	for (int i=0; i < num_classes; i++)
+	{
+		scores[i] = scores[i] / total;
+	}
+	return scores;
+}
+
+int
+Regressor::predictLabel(std::vector<float> features){
+	int label = -1;
+	float current_score = -1;
+	std::vector<float> scores = softmax(features);
+	for (int i=0; i < num_classes; i++)
+	{
+		if (current_score < scores[i])
+		{
+			current_score = scores[i];
+			label = i;
+		}
+	}
+	return label;
 }
